d1d563d0dba4f63042ada5c1a2fd8f5558c04022
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-01-17 06:07:32 -0300

    🐛 Bug: create a loop to keep infinite

--- src/app/action.tsx
+++ src/app/action.tsx
@@ -1,12 +1,17 @@
 "use server";
 
 import AnimeCard, { type AnimeProp } from "@/components/AnimeCard";
-
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
     `https://shikimori.one/api/animes?page=${page}&limit=8&season=2024&order=popularity`,
   );
   const data = await response.json();
+
+  if (data.length < 5) {
+    return fetchAnime(page - 10);
+  }
 
   return data.map((item: AnimeProp, index: number) => (
     <AnimeCard key={item.id} anime={item} index={index} />
