4a772587af09f73650a99a276759c9a6dcb298c5
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-01-17 04:08:10 -0300

    ✨ Feat: fetch and use data from server action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>ola mundo</h1>;
+import AnimeCard, { type AnimeProp } from "@/components/AnimeCard";
+import LoadMore from "../components/LoadMore";
+
+import { fetchAnime } from "./action";
+
+async function Home() {
+  const data = await fetchAnime(1);
+  return (
+    <main className="flex flex-col gap-10 px-8 py-16 sm:p-16">
+      <h2 className="text-3xl font-bold text-white">Lista de Anime</h2>
+
+      <section className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <LoadMore />
+    </main>
+  );
 }
+
+export default Home;
