b5ef31fe4669da8b84da90ed7587fa3c18a70b33
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2024-01-17 04:33:04 -0300

    ✨ Feat: implement infinite scroll

--- src/components/LoadMore.tsx
+++ src/components/LoadMore.tsx
@@ -1,10 +1,34 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+"use client";
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { type AnimeProp } from "./AnimeCard";
+
+let page = 2;
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
+      <section className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex w-full items-center justify-center">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
